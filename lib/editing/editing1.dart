2236199c41cc247d3c7a5430ec0048446d86e58f
Nurdiananur <kylychbek_kyzy_n@iuca.kg>
2022-04-20 05:12:19 +0600

    new

--- lib/editing/editing1.dart
+++ lib/editing/editing1.dart
@@ -1,3 +1,4 @@
+import 'package:cargo_app/styles/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class Editing1 extends StatefulWidget {
@@ -17,6 +18,7 @@ class _Editing1State extends State<Editing1> {
       body: Padding(
         padding: const EdgeInsets.only(left: 16, right: 16, top: 32),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text('Имя'),
             Container(
@@ -26,7 +28,147 @@ class _Editing1State extends State<Editing1> {
                 borderRadius: BorderRadius.circular(10),
                 color:Color(0xffF3F3F3 )
               ),
+              child: TextField(
+                autofocus: false,
+                decoration:
+                InputDecoration(
+                    hintText: "Марина",
+                    // border: InputBorder.none,
+
+                ),
+
+              ),
+            ),
+            SizedBox(height: 15,),
+
+            Text('Фамилия'),
+            Container(
+              width: 343,
+              height: 50,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color:Color(0xffF3F3F3 )
+              ),
+              child: TextField(
+                autofocus: false,
+                decoration:
+                InputDecoration(
+                  hintText: "Ивановна",
+                  // border: InputBorder.none,
+
+                ),
+
+              ),
+            ),
+            SizedBox(height: 15,),
+
+            Text('Номер телефона'),
+            Container(
+              width: 343,
+              height: 50,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color:Color(0xffF3F3F3 )
+              ),
+              child: TextField(
+                autofocus: false,
+                decoration:
+                InputDecoration(
+                  hintText: "+996 705 779 778",
+                  // border: InputBorder.none,
+
+                ),
+
+              ),
+            ),
+            SizedBox(height: 15,),
+            Text('Место проживания'),
+            Container(
+              width: 343,
+              height: 50,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color:Color(0xffF3F3F3 )
+              ),
+              child: TextField(
+                autofocus: false,
+                decoration:
+                InputDecoration(
+                  hintText: "Кыргызстан,Бишкек",
+                  // border: InputBorder.none,
+
+                ),
+
+              ),
+            ),
+
+            SizedBox(height: 15,),
+            Text('Дата рождения'),
+            Container(
+              width: 343,
+              height: 50,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color:Color(0xffF3F3F3 )
+              ),
+              child: TextField(
+                autofocus: false,
+                decoration:
+                InputDecoration(
+                  hintText: "23-09-1995",
+                  // border: InputBorder.none,
+
+                ),
+
+              ),
+            ),
+
+            SizedBox(height: 15,),
+            Text('Пол'),
+            Container(
+              width: 343,
+              height: 50,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color:Color(0xffF3F3F3 )
+              ),
+              child: TextField(
+                autofocus: false,
+                decoration:
+                InputDecoration(
+                  hintText: "Женский",
+                    suffixIcon: Icon(Icons.keyboard_arrow_down_sharp),
+
+
+
+
+                ),
+
+              ),
+            ),
+            GestureDetector(
+              onTap: (){
+                // Navigator.push(
+                //     context,
+                //     MaterialPageRoute(
+                //       builder: (context) => const Editing(),
+                //     )
+                // );
+              },
+              child: Container(
+                width: 343,
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue
+                ),
+                child: Center(
+                  child: Text('Редактировать', style: AppTextStyles.textbottom,),
+                ),
+              ),
+
             )
+
           ],
 
         ),
