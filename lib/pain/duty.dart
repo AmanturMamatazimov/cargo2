f60304a402473197857e021608fbcac44321f514
Nurdiananur <kylychbek_kyzy_n@iuca.kg>
2022-05-09 19:59:07 +0600

    ios adaptive

--- lib/pain/duty.dart
+++ lib/pain/duty.dart
@@ -1,3 +1,4 @@
+import 'package:cargo_app/styles/app_colors.dart';
 import 'package:cargo_app/styles/app_text_styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,127 +11,221 @@ class Duty extends StatefulWidget {
 }
 
 class _DutyState extends State<Duty> {
-  @override
 
+
+  Widget _matables(String name) {
+    return Container(
+      width: 156,
+      height: 50,
+      decoration: BoxDecoration(
+        color: AppColors.table,
+        border: Border.all(color: Colors.black38),
+
+      ),
+      child: Center(
+        child: Text(name),
+      ),
+    );
+  }
+
+  Widget _mytable(String name) {
+    return Container(
+      width: 156,
+      height: 50,
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.white70),
+          color: Colors.blue,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(10))
+      ),
+      child: Center(
+        child: Text(name, style: TextStyle(color: Colors.white),),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Долг',style: TextStyle(color: Colors.red
-        ),),
+        title: Text('Долг', style: TextStyle(color: Colors.red),),
       ),
-      body: SingleChildScrollView(
+      body: Center(
+        child: ListView(
+          padding: EdgeInsets.all(20),
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 32, left: 106, right: 106),
+              child: Container(
+                width: 163,
+                height: 38,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.red
+                ),
+                child: Center(child: Text(
+                  '40 000 руб', style: AppTextStyles.textbottom,)),
+              ),
+            ),
+            SizedBox(height: 16,),
+            Table(
+
+              children: <TableRow>[
+
+                TableRow(
+                  children: <Widget>[
+
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              _mytable('Дата Отправки'),
+                              _mytable('Дата Получения'),
+                              _mytable("Рейс"),
+                              _mytable("Сумма")
 
 
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
 
-         child: Center(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 32, left: 16, right: 16,bottom: 10 ),
-            child: Column(
-              children: [
-                Column(
-                   children: [
-                    Container(
-                    width: 163,
-                    height: 38,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.red
+                              _matables('24.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('3 000 руб'),
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              _matables('26.02.22'),
+                              _matables('28.02.22'),
+                              _matables('TK 428'),
+                              _matables('6 000 руб'),
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+                            ],
+                          ),
+
+                        ],
+                      ),
                     ),
-                    child: Center(child: Text('40 000 руб',style: AppTextStyles.textbottom,)),
-                  ),
-
-                SizedBox(height: 11,),
-
-                          Table(
-
-                          border: TableBorder.all(color: Colors.blue),
-                          columnWidths: {
-                            0: FractionColumnWidth(0.4),
-
-                          },
-                          children: [
-                            TableRow(children: [
-                              Container(
-                                width: 155,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                  color: Colors.blue,
-                                  borderRadius: BorderRadius.circular(10)
-                                ),
-                                child: Center(
-                                 child: Text('Дата Отправки'),
-                                ),
-                              ),
-                              Container(
-                                width: 155,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                    color: Colors.blue,
-                                    borderRadius: BorderRadius.circular(10)
-                                ),
-                                child: Center(
-                                  child: Text('Дата Отправки'),
-                                ),
-                              ),
-                              Container(
-                                width: 155,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                    color: Colors.blue,
-                                    borderRadius: BorderRadius.circular(10)
-                                ),
-                                child: Center(
-                                  child: Text('Дата Отправки'),
-                                ),
-                              ),
-                              // Container(
-                              //   width: 155,
-                              //   height: 50,
-                              //   decoration: BoxDecoration(
-                              //       color: Colors.blue,
-                              //       borderRadius: BorderRadius.circular(10)
-                              //   ),
-                              //   child: Center(
-                              //     child: Text('Дата Отправки'),
-                              //   ),
-                              // )
-
-
-                            ]),
-
-                            buildRow(['Дата Отправки', 'Дата Получения', 'Рейс', ], isHeader: true),
-                            buildRow(['24.02.22', '26.02.22', 'SU 123', ]),
-                            buildRow(['24.02.22', '26.02.22', 'SU 123', ]),
-                            buildRow(['24.02.22', '26.02.22', 'SU 123', ]),
-                            buildRow(['24.02.22', '26.02.22', 'SU 123', ]),
-                            buildRow(['24.02.22', '26.02.22', 'SU 123', ]),
-                            buildRow(['24.02.22', '26.02.22', 'SU 123',]),
-                            buildRow(['24.02.22', '26.02.22', 'SU 123', ]),
-                            buildRow(['24.02.22', '26.02.22', 'SU 123', ]),
-                            buildRow(['24.02.22', '26.02.22', 'SU 123',]),
-
-
-                          ],
-                  ),
-                    ]
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
-           ]
-    )
-      )
-    )
-    )
+      ),
     );
-
   }
-  TableRow buildRow(List<String> cells, {bool isHeader = false}) => TableRow(
-children: cells.map((cell) {
-  final style = TextStyle(
-    fontWeight: isHeader ? FontWeight.bold : FontWeight.normal,
-    fontSize: 18,
-  );
-  return Padding(padding: const EdgeInsets.all(12),
-  child: Center(child: Text(cell, style: style,),),
-  );
-} ).toList()
-  );
 }
--- lib/pain/paid.dart
+++ lib/pain/paid.dart
@@ -1,3 +1,6 @@
+
+
+import 'package:cargo_app/styles/app_colors.dart';
 import 'package:cargo_app/styles/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
@@ -9,120 +12,225 @@ class Paid extends StatefulWidget {
 }
 
 class _PaidState extends State<Paid> {
+
+
+  Widget _matables (String name){
+    return Container(
+      width: 156,
+      height: 50,
+      decoration: BoxDecoration(
+        color: AppColors.table,
+        border: Border.all(color: Colors.black38),
+
+      ),
+      child: Center(
+        child: Text(name),
+      ),
+    );
+  }
+  Widget _mytable(String name){
+    return Container(
+      width: 156,
+      height: 50,
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.white70),
+          color: Colors.blue,
+          borderRadius: BorderRadius.vertical( top: Radius.circular(10))
+      ),
+      child: Center(
+        child: Text(name, style: TextStyle(color: Colors.white),),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
-        title: Text('Оплачено'),
+        title: Text('Оплачено', style: TextStyle(color: Colors.green),),
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 32, left: 16, right: 16),
-            child: Column(
-              children: [
-                Container(
-                  width: 163,
-                  height: 38,
-                  decoration: BoxDecoration(
+      body: Center(
+        child: ListView(
+          padding: EdgeInsets.all(20),
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 32, left: 106, right: 106),
+              child: Container(
+                width: 163,
+                height: 38,
+                decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
                     color: Colors.green
-                  ),
-                  child: Center(child: Text('50 000 руб',style: AppTextStyles.textbottom,)),
                 ),
+                child: Center(child: Text('50 000 руб',style: AppTextStyles.textbottom,)),
+              ),
+            ),
+            SizedBox(height: 16,),
+            Table(
 
-    SizedBox(height: 11,),
+              children: <TableRow>[
 
-    Table(
+                TableRow(
+                  children: <Widget>[
 
-    border: TableBorder.all(color: Colors.blue),
-    columnWidths: {
-    0: FractionColumnWidth(0.4),
-
-    },
-    children: [
-    TableRow(children: [
-    Container(
-    width: 155,
-    height: 50,
-    decoration: BoxDecoration(
-    color: Colors.blue,
-    borderRadius: BorderRadius.circular(10)
-    ),
-    child: Center(
-    child: Text('Дата Отправки'),
-    ),
-    ),
-    Container(
-    width: 155,
-    height: 50,
-    decoration: BoxDecoration(
-    color: Colors.blue,
-    borderRadius: BorderRadius.circular(10)
-    ),
-    child: Center(
-    child: Text('Дата Отправки'),
-    ),
-    ),
-    Container(
-    width: 155,
-    height: 50,
-    decoration: BoxDecoration(
-    color: Colors.blue,
-    borderRadius: BorderRadius.circular(10)
-    ),
-    child: Center(
-    child: Text('Дата Отправки'),
-    ),
-    ),
-    // Container(
-    //   width: 155,
-    //   height: 50,
-    //   decoration: BoxDecoration(
-    //       color: Colors.blue,
-    //       borderRadius: BorderRadius.circular(10)
-    //   ),
-    //   child: Center(
-    //     child: Text('Дата Отправки'),
-    //   ),
-    // )
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              _mytable('Дата Отправки'),
+                              _mytable('Дата Получения'),
+                              _mytable("Рейс"),
+                              _mytable("Сумма")
 
 
-    ]),
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
 
-    buildRow(['Дата Отправки', 'Дата Получения', 'Рейс', ], isHeader: true),
-    buildRow(['24.02.22', '26.02.22', 'SU 123', ]),
-    buildRow(['24.02.22', '26.02.22', 'SU 123', ]),
-    buildRow(['24.02.22', '26.02.22', 'SU 123', ]),
-    buildRow(['24.02.22', '26.02.22', 'SU 123', ]),
-    buildRow(['24.02.22', '26.02.22', 'SU 123', ]),
-    buildRow(['24.02.22', '26.02.22', 'SU 123',]),
-    buildRow(['24.02.22', '26.02.22', 'SU 123', ]),
-    buildRow(['24.02.22', '26.02.22', 'SU 123', ]),
-    buildRow(['24.02.22', '26.02.22', 'SU 123',]),
+                              _matables('24.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('3 000 руб'),
 
 
-    ],
-    ),
-    ]
-    ),
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              _matables('26.02.22'),
+                              _matables('28.02.22'),
+                              _matables('TK 428'),
+                              _matables('6 000 руб'),
 
-    )
-    ),
-      )
-      );
 
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              _matables('26.02.22'),
+                              _matables('26.02.22'),
+                              _matables('SU 123'),
+                              _matables('8 000 руб'),
+
+
+
+                            ],
+                          ),
+
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
-  TableRow buildRow(List<String> cells, {bool isHeader = false}) => TableRow(
-      children: cells.map((cell) {
-        final style = TextStyle(
-          fontWeight: isHeader ? FontWeight.bold : FontWeight.normal,
-          fontSize: 18,
-        );
-        return Padding(padding: const EdgeInsets.all(12),
-          child: Center(child: Text(cell, style: style,),),
-        );
-      } ).toList()
-  );
+
 }
